f313c359d7c84f0479714a63dca9e679e7ac1241
Erik Tran <erik.tran.tt@gmail.com>
2020-11-17 16:29:23 +1100

    add: input form control

--- src/containers/chat_room.jsx
+++ src/containers/chat_room.jsx
@@ -14,15 +14,35 @@ const Chat = (props) => {
   );
 };
 
+const Input = () => {
+  return (
+    <div className="input">
+      <form className="form-inline">
+        <div className="form-row">
+          <input type="text" name="message" className="form-control" id="message-input" />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const renderChatList = () => {
+  return (
+    <div className="chat-list horizontal-line">
+      <Chat sender="ssaunier" content="Hellow world!!" time="20:10:45" />
+      <Chat sender="edward" content="Hi :)" time="20:11:15" />
+      <Chat sender="papillard" content="How are you guys? Having fun with React and Redux?" time="20:11:55" />
+    </div>
+  );
+};
+
 const ChatRoom = (props) => {
   return (
     <section className="chatroom">
       <h2 className="headers horizontal-line">Channel #general</h2>
-      <div className="chat-list">
-        <Chat sender="ssaunier" content="Hellow world!!" time="20:10:45" />
-        <Chat sender="edward" content="Hi :)" time="20:11:15" />
-        <Chat sender="papillard" content="How are you guys? Having fun with React and Redux?" time="20:11:55" />
-      </div>
+      { renderChatList() }
+      <Input />
     </section>
   );
 };
