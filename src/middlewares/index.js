23397f4386410d70f4837af9d08ae09c1396d643
Erik Tran <erik.tran.tt@gmail.com>
2020-11-17 16:09:08 +1100

    added Redux components

---
+++ src/middlewares/index.js
@@ -0,0 +1,6 @@
+import { applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+
+const middlewares = applyMiddleware(logger);
+
+export default middlewares;
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+// State and reducers
+const reducers = combineReducers({
+  changeMe: (state = null, action) => state
+});
+
+export default reducers;
