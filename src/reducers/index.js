b24b0bddbb8beac76b66ca9f453291b8c8f84db8
Erik Tran <erik.tran.tt@gmail.com>
2020-11-18 01:00:51 +1100

    add: root reducers and relating reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
+import messagesReducer from './messages_reducer';
+import selectedChannelReducer from './selected_channel_reducer';
+
+// initial State
+export const initialState = {
+  messages: [],
+  channels: ['general', 'react', 'paris'],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
+const identityReducer = (state = null) => state;
 
 // State and reducers
-const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+export const reducers = combineReducers({
+  messages: messagesReducer,
+  channels: identityReducer,
+  currentUser: identityReducer,
+  selectedChannel: selectedChannelReducer
 });
-
-export default reducers;
