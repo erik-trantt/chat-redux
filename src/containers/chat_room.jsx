ba0e525513b7e1cea56c762980b03ba96832d850
Erik Tran <erik.tran.tt@gmail.com>
2020-11-20 23:52:40 +1100

    wip: refactoring ChatRoom component into class component

--- src/containers/chat_room.jsx
+++ src/containers/chat_room.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { selectChannel, fetchMessages } from '../actions/index';
 
-const Chat = (props) => {
+const Message = (props) => {
+  const { created_at, author, content } = props.message;
+  const time = new Date(created_at).toLocaleTimeString();
   return (
     <div className="chat">
       <div className="head">
-        <span className="sender">{props.sender}</span>
-        <span className="time"> - {props.time}</span>
+        <span className="sender">{author}</span>
+        <span className="time"> - {time}</span>
       </div>
       <div className="content">
-        {props.content}
+        {content}
       </div>
     </div>
   );
@@ -27,24 +32,43 @@ const Input = () => {
   );
 };
 
-const renderChatList = () => {
-  return (
-    <div className="chat-list horizontal-line">
-      <Chat sender="ssaunier" content="Hellow world!!" time="20:10:45" />
-      <Chat sender="edward" content="Hi :)" time="20:11:15" />
-      <Chat sender="papillard" content="How are you guys? Having fun with React and Redux?" time="20:11:55" />
-    </div>
-  );
-};
+class ChatRoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-const ChatRoom = (props) => {
-  return (
-    <section className="chatroom">
-      <h2 className="headers horizontal-line">Channel #general</h2>
-      { renderChatList() }
-      <Input />
-    </section>
-  );
-};
+  renderChatList = () => {
+    return (
+      <div className="chat-list horizontal-line">
+        <Message message={{ author: "ssaunier", content: "Hellow world!!", created_at: "2020-10-26T20:10:45" }} />
+        <Message message={{ author: "edward", content: "Hi :)", created_at: "2020-10-26T20:11:15" }} />
+        <Message message={{ author: "papillard", content: "How are you guys? Having fun with React and Redux?", created_at: "2020-10-26T20:11:55" }} />
+      </div>
+    );
+  }
 
-export default ChatRoom;
+  render() {
+    return (
+      <section className="chatroom">
+        <h2 className="headers horizontal-line">Channel #general</h2>
+        { this.renderChatList() }
+        <Input />
+      </section>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    messages: state.messages,
+    selectedChannel: state.selectedChannel
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchMessages }, dispatch);
+}
+
+// export default ChatRoom;
+export default connect(mapStateToProps, mapDispatchToProps)(ChatRoom);
